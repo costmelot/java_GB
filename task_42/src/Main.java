36259f48ba7148bd19e6b4f408b734e4e35054ad
Andrey <costmelot@mail.ru>
2022-12-22 23:13:58 +0400

    The task_42 was created and added.

--- task_42/src/Main.java
+++ task_42/src/Main.java
@@ -1,3 +1,6 @@
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//        enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его,
+//        first() - возвращает первый элемент из очереди, не удаляя.
 class LinkedListQueue {
     private Node front, rear;
     private int queueSize;
